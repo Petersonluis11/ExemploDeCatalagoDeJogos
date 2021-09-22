c7043f60bbd0318bc166d049cd0598124b793071
Peterson <petersonluisalves.pla@gmail.com>
2021-09-22 00:19:22 -0300

    ExemploDeCatalagoDeJogos

---
+++ JogoJaCadastradoExceptions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace jogo
+{
+    public class JogoJaCadastradoExceptions
+    {
+        
+     public JogoJaCadastradoException()
+            : base("Este já jogo está cadastrado")
+        { }
+    }
+}
---
+++ JogoNaoCadastradoExceptions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace jogo
+{
+    public class JogoNaoCadastradoExceptions
+    {
+        
+    public JogoNaoCadastradoException()
+            :base("Este jogo não está cadastrado")
+        {}
+    }
+}
